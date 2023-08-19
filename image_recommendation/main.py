2fc7adc4c40fc42792cd17b0e83a292ceaeabde4
Yseo_moon <68218789+copel0317@users.noreply.github.com>
2023-08-20 05:35:29 +0900

    Update main.py

--- image_recommendation/main.py
+++ image_recommendation/main.py
@@ -5,24 +5,35 @@ from calculator import Recommendation
 
 import pandas as pd
 
-
 #기존 dataframe 호출
-path="/home/user/SaGol/MiniGPT-4/image_recommendation/MinigptOut.csv"
-data=pd.read_csv(path)  
+def getPictures(path):
+    data=pd.read_csv(path)
+    return data
 
-#모델 돌리기
-photopath="/home/user/SaGol/MiniGPT-4/Images"
-g = Generator(data)
-g.generate(photopath)
+def getAltText(data,photopath):
+    #텍스트 생성
+    g = Generator(data)
+    g.generate(photopath)
+    #토큰 처리 및 TF-IDF 알고리즘 수행
+    data=Database(g.data)
+    return data
 
-#토큰 처리 및 TF-IDF 알고리즘
-data=Database(g.data)
-print(data.data)  #디버깅용
+def getBestPictures(userText,data):
+    q=Query(userText, data)
+    r=Recommendation(q,data)
+    r.printf()
+    return r
 
-#사용자 질문을 처리하는 부분.
-userText = "펭귄 세 마리가 찍혀있는 사진을 찾아 줘."
-q=Query(userText, data)
-r=Recommendation(q,data)
+if __name__ == '__main__':
+    datapath="/home/user/SaGol/MiniGPT-4/image_recommendation/MinigptOut.csv"
+    photopath="/home/user/SaGol/MiniGPT-4/Images"
+    userText = "펭귄 세 마리가 찍혀있는 사진을 찾아 줘."
+    
+    #기존 dataframe 호출
+    data=getPictures(datapath)
+    
+    #모델 돌리기
+    data=getAltText(data,photopath)
 
-#출력
-r.printf()
+    #사용자 질문을 처리하는 부분.
+    getBestPictures(userText,data)
